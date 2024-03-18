08f016200ad9e38532bd597dd046917c6b020151
Iyobed Zekarias <iyobedzekarias@gmail.com>
2024-03-18 15:23:27 -0400

    blur change

---
+++ app/components/client/landing-image.tsx
@@ -0,0 +1,45 @@
+'use client'
+import Image from "next/image"
+import { useEffect, useState } from "react";
+
+export default function LandingImage() {
+    const [scrollLevel, setScroll] = useState(0)
+
+    const handleScroll = () => {
+        const scrollPosition = window.scrollY; // => scroll position
+        if (scrollPosition < 20) {
+            setScroll(0)
+        } else if (scrollPosition < 40) {
+            setScroll(1)
+        } else if (scrollPosition < 60) {
+            setScroll(2)
+        } else if (scrollPosition < 80) {
+            setScroll(3)
+        } else if (scrollPosition < 100) {
+            setScroll(4)
+        }
+        console.log(scrollLevel)
+    };
+
+    useEffect(() => {
+        handleScroll();
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <Image
+            src="/logo.svg"
+            alt=""
+            width={50}
+            height={50}
+            className={`w-7/12 opacity-50 ${scrollLevel == 0 ? "blur-lg" :
+                scrollLevel == 1 ? "blur-md" :
+                    scrollLevel == 2 ? "blur" :
+                        scrollLevel == 3 ? "blur-sm" : "blur-none"} transition-blur ease-in-out delay-[3000ms] active:blur-none`}
+            priority
+        />
+    )
+}
--- app/components/landing.tsx
+++ app/components/landing.tsx
@@ -1,18 +1,12 @@
 import Image from "next/image"
 import DownArrow from "./client/down-arrow"
 import EmailButton from "./client/email-button"
+import LandingImage from "./client/landing-image"
 
 export default function Landing() {
     return (
         <div className="flex flex-row-reverse w-screen h-screen items-center relative z-0">
-            <Image
-                src="/logo.svg"
-                alt=""
-                width={50}
-                height={50}
-                className="w-7/12 opacity-50 blur-lg transition-blur ease-in-out delay-[3000ms] hover:blur-none active:blur-none"
-                priority
-            />
+            <LandingImage />
             <div className="absolute inset-0 w-6/12 md:2/12 flex flex-col justify-center z-10">
                 <span className="flex-1 ml-10 md:ml-40 mt-40 "></span>
                 <p className="ml-10 md:ml-40 text-4xl md:text-5xl lg:text-7xl font-bold font-georgiaSerif text-pretty">A Focus on Truth for <i>Valuation+</i></p>
